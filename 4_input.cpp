c2f3e14b305b6d23066bc89907849ae32c6326fb
Tanishq <koul.tanishq111@gmail.com>
2024-04-15 22:47:41 +0530

    4 input and out put

---
+++ 4_input.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int amount1;
+    cout<<"Enter the Amount given buy dad... : ";
+    cin >> amount1;
+
+    int amount2;
+        cout<<"Enter the Amount given buy mom... : ";
+        cin >> amount2;
+
+        int sum = amount1 + amount2;
+        cout<<"sum\n";
+        cout<<sum;
+
+        return 0;
+}

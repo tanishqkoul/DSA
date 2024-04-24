8a268c59e1f9c983d9eaa0c17fc14fe54c8db837
Tanishq <koul.tanishq111@gmail.com>
2024-04-25 00:42:28 +0530

    half pyramid

---
+++ PQ6.cpp
@@ -0,0 +1,17 @@
+// Print Half Pyramid using numbers
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cout<<"Enter the number :";
+    cin>>n;
+
+    for (int i = 1; i<= n; i++){
+        for (int j = 1 ; j <= i ; ++j){
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}

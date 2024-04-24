55e945d956c2784edef8a2bbcfdad5f1a27a0515
Tanishq <koul.tanishq111@gmail.com>
2024-04-25 00:47:43 +0530

    HP using numbers

--- PQ7.cpp
+++ PQ7.cpp
@@ -1,1 +1,16 @@
-PD
+#include <iostream>
+using namespace std;
+// Print Inverted Half Pyramid using numbers
+int main(){
+    int n ;
+    cout <<"Enter the number :";
+    cin >> n;
+
+    for(int i = n ; i>=1; --i){
+        for(int j = 1; j<=i; ++j){
+            cout<< j << " ";
+    }
+    cout<<endl;
+}
+return 0;
+}

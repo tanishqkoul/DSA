a4d696e20592d1e985a3aaf166dfeb96433f33dc
Tanishq <koul.tanishq111@gmail.com>
2024-04-26 01:18:17 +0530

    PQ11

--- PQ11.cpp
+++ PQ11.cpp
@@ -1,7 +1,20 @@
-// Print 0-1 pattern
 #include<iostream>
 using namespace std;
 
 int main(){
-    
+    int i,j,r;
+    cout <<"Enter the value of r :";
+    cin>>r;
+
+    for(i=1;i<=r;i++){
+        for(j=1;j<=i;j++){
+            if((i+j) %2 ==0)
+                cout<<"1 ";
+            else{
+                cout<<"0 ";
+            }
+        }
+        cout<<endl; // Move this line outside of the inner loop
+    }
+    return 0;
 }
